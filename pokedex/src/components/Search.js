7aa2337740c20b305060d318daefc5ba5536829a
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-08-01 13:19:55 +0100

    search function now working

--- pokedex/src/components/Search.js
+++ pokedex/src/components/Search.js
@@ -1,10 +1,45 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Container, Row } from 'react-bootstrap';
+
+import Pokemon from "./Pokemon";
 import SearchBar from "./SearchBar";
 
 const Search = () => {
-    
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [getPokemon, setPokemon] = useState('');
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
+            setPokemon(res.data.name);
+        }
+        fetchPosts();
+    }, [searchTerm]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchTerm(e.target.value);
+    }
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
     return (
         <>
-            <SearchBar/>
+            <SearchBar handlesubmit={handleSubmit} handleChange={handleChange} />
+
+            {
+                searchTerm === ''
+                    ? <></>
+                    : <Container id='pokemonResult' className="d-flex vw-100">
+                        <Row className="m-auto">
+                            <Pokemon key={getPokemon} name={getPokemon} />
+                        </Row>
+                    </Container>
+            }
         </>
     )
 };
--- pokedex/src/components/SearchBar.js
+++ pokedex/src/components/SearchBar.js
@@ -6,8 +6,8 @@ const SearchBar = (props) => {
                     <form action='' onSubmit={props.handleSubmit} >
                         <div className='input-field'>
                             <label>Name or Number</label><br/>
-                            <input type='text' />
-                            <i className="fa-solid fa-magnifying-glass"></i>
+                            <input type='text' onChange={props.handleChange}/>
+                            <i className="fa-solid fa-magnifying-glass" onClick={props.handleSubmit}></i>
                         </div>
                     </form>
             </div>
